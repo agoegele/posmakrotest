f657955301c1b74f41bee45cec0eeafbdc359034
agoegele <61741645+agoegele@users.noreply.github.com>
2021-05-03 16:41:18 +0200

    Add files via upload

---
+++ buchhandlung-service/src/main/java/at/itkollegimst/goegele/pos1makro/test2/druckerei/BuchhandlungApplication.java
@@ -0,0 +1,13 @@
+package at.itkollegimst.goegele.pos1makro.test2.druckerei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuchhandlungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuchhandlungApplication.class, args);
+	}
+
+}
---
+++ buchhandlung-service/src/test/java/at/itkollegimst/goegele/pos1makro/test2/druckerei/BuchhandlungApplicationTests.java
@@ -0,0 +1,13 @@
+package at.itkollegimst.goegele.pos1makro.test2.druckerei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuchhandlungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
